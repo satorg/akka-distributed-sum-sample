6a4bcf0181b6fefb2358fccc260076bcfd6351dc
Sergey Torgashov <storgashov@qubell.com>
2017-09-27 15:04:15 +0400

    initial "quick&durty" worker implementation

---
+++ src/main/scala/satorg/sample/akka/dsum/WorkerActor.scala
@@ -0,0 +1,58 @@
+package satorg.sample.akka.dsum
+
+import akka.actor.{Actor, ActorLogging, Props}
+
+object WorkerActor {
+  def props(id: Int, idMax: Int, num: Long): Props = Props(new WorkerActor(id, idMax, num))
+
+  def makeName(id: Int) = s"worker-$id"
+
+  case class ReceiveNum(senderId: Int, senderNum: Long)
+
+}
+
+import satorg.sample.akka.dsum.WorkerActor._
+
+class WorkerActor(myId: Int, maxId: Int, myNum: Long) extends Actor with ActorLogging {
+
+  private var resultNum: Long = myNum
+
+  private var childIds: Set[Int] = {
+    val childId1 = myId * 2
+    val childId2 = childId1 + 1
+
+    (childId1 :: childId2 :: Nil).iterator.takeWhile(_ <= maxId).toSet
+  }
+
+  override def preStart(): Unit = {
+    sendResult()
+  }
+
+  override def receive: Receive = {
+    case ReceiveNum(senderId, senderNum) if childIds.contains(senderId) =>
+      log.debug("received {} from child ID={}", senderNum, senderId)
+      resultNum += senderNum
+      childIds -= senderId
+      sendResult()
+
+    case ReceiveNum(senderId, _) =>
+      log.warning("unexpected sender ID={}", senderId)
+  }
+
+  private def sendResult(): Unit = {
+    if (childIds.nonEmpty)
+      return
+
+    if (myId == 1) {
+      // TODO: send result to the caller.
+      log.info("Result: {}", resultNum)
+      return
+    }
+
+    val parentId: Int = myId / 2
+    val parentPath = context.self.path.parent / makeName(parentId)
+
+    log.debug(s"sending {} to parent ID={}", resultNum, parentId)
+    context.actorSelection(parentPath) ! ReceiveNum(myId, resultNum)
+  }
+}
