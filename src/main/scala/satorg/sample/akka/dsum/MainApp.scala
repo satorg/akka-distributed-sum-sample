a522a267b252090f223c9b25c9005a9df5c4f6d2
Sergey Torgashov <storgashov@qubell.com>
2017-09-27 15:49:25 +0400

    reading numbers from stdin

--- src/main/scala/satorg/sample/akka/dsum/MainApp.scala
+++ src/main/scala/satorg/sample/akka/dsum/MainApp.scala
@@ -6,9 +6,12 @@ import akka.util.Timeout
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.io.StdIn
 
 object MainApp {
   def main(args: Array[String]): Unit = {
+    val nums = readNums()
+
     val maxWorkDuration = 1.minute
     implicit val workTimeout: Timeout = maxWorkDuration
 
@@ -16,14 +19,16 @@ object MainApp {
     try {
       val managerActorRef = system.actorOf(ManagerActor.props, "manager")
 
-      val nums = 100L to 110L
       val workFuture = (managerActorRef ? ManagerActor.StartWork(nums)).mapTo[ManagerActor.ReturnResult]
-
       val result = Await.result(workFuture, maxWorkDuration)
       println(s"Result: ${result.num}")
     }
     finally {
       system.terminate()
     }
+  }
+
+  private def readNums(): Seq[Long] = {
+    Iterator.continually(StdIn.readLine()).takeWhile(_ != null).map(_.toLong).toVector
   }
 }
